22b200cfa728aefd681267ad2f26b3245b7dd326
Jose Manuel <josemanuelharri@gmail.com>
2025-06-24 22:12:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import CardPizza from './components/CardPizza'
+import Home from './components/Home'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+   
+     
+    </>
+  )
+}
+
+export default App
